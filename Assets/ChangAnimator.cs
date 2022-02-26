d68f87463dbc1ba5d11194916ec4e7bffe4676a9
chen210843 <siriwut.phuwanasanti@gmail.com>
2022-02-26 20:40:26 +0700

    google

---
+++ Assets/ChangAnimator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void getAnimatorDefeate(){
+        this.GetComponent<Animator>().Play("aj@Big Kidney Hit");
+    }
+
+    public void  getAnimatorRumba(){
+        this.GetComponent<Animator>().Play("aj@Flip Kick");
+    }
+}
---
+++ Assets/ChangColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    public void getRed(){
+         GetComponent<Renderer>().material.color = Color.red; 
+    }
+    public void getGreen(){
+        GetComponent<Renderer>().material.color = Color.green; 
+    }
+    public void getBlue(){
+        GetComponent<Renderer>().material.color = Color.blue; 
+    }
+}
+
